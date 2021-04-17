ceb6e9d23d369f64bac4c797d0be8b263a4654c6
Dalejan <david1804997@gmail.com>
2021-04-17 09:43:42 -0500

    Added tensorflow vm

---
+++ Practice1_Vagrant/bootstrapServidor.sh
@@ -0,0 +1,3 @@
+sudo apt install python3-pip -y
+pip3 install jupyter
+export PATH="$HOME/.local/bin:$PATH"
--- test2_box/.vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.29.0"]]],"checksum":"8763fe0214cc7854b9af76e34e996cb6f96e4033cf3544cbc9e2db5a87c9586c","vagrant_version":"2.2.15"}
