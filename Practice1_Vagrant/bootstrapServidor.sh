f1c8c032a1885a8c802fb40c5b63121bedc77f5e
Dalejan <david1804997@gmail.com>
2021-04-17 22:03:05 -0500

    Added NLP notebooks

--- Practice1_Vagrant/bootstrapServidor.sh
+++ Practice1_Vagrant/bootstrapServidor.sh
@@ -1,4 +1,8 @@
 sudo apt install python3-pip -y
 pip3 install jupyter
+pip3 install -U nltk
+pip3 install pandas
 
+pip3 install facets-overview==1.0.0
+pip3 install tensorflow
 export PATH="$HOME/.local/bin:$PATH"
